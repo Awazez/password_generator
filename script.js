35e733a92bd390aabde5b55abe36bf16206c35a1
Martin AUBEUT <martin.aubeut@gmail.com>
2022-06-11 17:47:43 +0200

    modified algo

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
+const password_ele = document.getElementById("pwd_txt");
+var string = "";
+const generate = document.getElementById("generate");
+const clipboard = document.getElementById("clipboard");
+var pwd_length = document.getElementById("slider");
 
-console.log("Hello, world!");
-document.getElementById("demo").innerHTML = generatePassword();
+function DisplayChange(newvalue) {
+    document.getElementById(
+      "value").innerHTML = newvalue;
+}
 
+clipboard.addEventListener('click', () => {
+    navigator.clipboard.writeText(password_ele.innerText);
+    alert("Password copied to clipboard");
+});
 
-function generatePassword(length = 16)
-{
-    let generatedPassword = "";
-
-    const validChars = "0123456789" +
-        "abcdefghijklmnopqrstuvwxyz" +
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-        ",.-{}+!\"#$%/()=?";
-
-    for (let i = 0; i < length; i++) {
-        let randomNumber = crypto.getRandomValues(new Uint32Array(1))[0];
-        randomNumber = randomNumber / 0x100000000;
-        randomNumber = Math.floor(randomNumber * validChars.length);
-
-        generatedPassword += validChars[randomNumber];
+generate.addEventListener('click', () => {
+    let password = "";
+    var checked = document.getElementById("checkboxSymbols").checked;
+    var checkedNumbers = document.getElementById("checkboxNumbers").checked;
+    var checkedLetters = document.getElementById("checkboxLetters").checked;
+    var checkedCapitalLetters = document.getElementById("checkboxCapitalLetters").checked;
+    var final_string = string;
+    console.log(checked);
+    if (checked) {
+        final_string += "!@#$%^&*";
+    }
+    if (checkedNumbers) {
+        final_string += "0123456789";
+    }
+    if (checkedLetters) {
+        final_string += "acdefghijklnopqrstuvwxyz";
     }
 
-    return generatedPassword;
-}
+    if (checkedCapitalLetters) {
+        final_string += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    for (let i = 0; i < pwd_length.value; i++) {
+        let randomNumber = crypto.getRandomValues(new Uint32Array(1))[0];
+        randomNumber = randomNumber / 0x100000000;
+        randomNumber = Math.floor(randomNumber * final_string.length);
+
+        password += final_string[randomNumber];
+    }
+    password_ele.innerText = password;
+    final_string = string;
+});
+
+clipboard.addEventListener('click', () => {
+    navigator.clipboard.writeText(password_ele.innerText);
+    alert("Password copied to clipboard");
+});
+
+
 
